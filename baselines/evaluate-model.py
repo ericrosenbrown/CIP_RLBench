88c8416e4489493b0a4202ac10af858675038311
SreehariRamMohan <sreehari.rammohan@gmail.com>
2021-11-02 13:43:33 -0400

    add evaluation script to baselines

---
+++ baselines/evaluate-model.py
@@ -0,0 +1,63 @@
+import gym
+import rlbench.gym
+import sys
+import os
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import utils_for_q_learning, buffer_class
+from ImageTaskWrapper import ImageTaskWrapper
+from button_RBFDQN_joint import Net
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("Running on the GPU")
+else:
+    device = torch.device("cpu")
+    print("Running on the CPU")
+
+hyper_parameter_name = sys.argv[1]
+alg = 'rbf'
+params = utils_for_q_learning.get_hyper_parameters(hyper_parameter_name, alg)
+params['hyper_parameters_name'] = hyper_parameter_name
+
+env = gym.make(params['env_name'], render_mode="human")
+
+#wrap around to decrease observation space
+env = ImageTaskWrapper(env)
+params['env'] = env
+params['seed_number'] = int(sys.argv[2])
+s0 = env.reset()
+
+# Load the trained agent
+Q_object = Net(params,
+                   env,
+                   state_size=len(s0),
+                   action_size=len(env.action_space.low),
+                   device=device)
+Q_object.load_state_dict(torch.load('/home/sreehari/Downloads/CIP_Results/vision_button_push_no_arm_init/logs/obj_net_button_push450_seed_0'))
+Q_object.eval()
+
+
+# Evaluate the agent
+num_episodes = 1000
+num_success = 0
+for j in range(num_episodes):
+    print("episode:", j)
+    obs = env.reset()
+
+    for i in range(200):
+        action = Q_object.e_greedy_policy(obs, j + 1, 'test')
+
+        obs, reward, done, info = env.step(action)
+
+        #print("action taken:", action, "finished? ", done)
+        
+        #env.render()
+        if done:
+            num_success += 1
+            break
+
+print("succes_rate = ", num_success/num_episodes )
