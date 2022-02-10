b26f9eb3c8e9daf4efc7baef0f1c94d01fd5e83a
Ben Abbatematteo <abba@brown.edu>
2022-02-10 17:28:30 -0500

    made subscriber

--- cip_cartesianimpedance.py
+++ cip_cartesianimpedance.py
@@ -1,9 +1,35 @@
 import rospy 
-from iiwa_msgs.msg import CartesianImpedanceControlMode
+from iiwa_msgs.msg import CartesianImpedanceControlMode, CartesianPose
 from iiwa_msgs.srv import ConfigureControlMode
+from geometry_msgs.msg import PoseStamped
+
+class ArmCommand(object):
+    """docstring for ArmCommand"""
+    def __init__(self):
+        self.x = None
+        self.y = None 
+        self.z = None
+        self.qx = None 
+        self.qy = None
+        self.qz = None
+        self.qw = None 
+        
+    def callback(self, msg):
+        self.x = msg.poseStamped.pose.position.x
+        self.y = msg.poseStamped.pose.position.y
+        self.z = msg.poseStamped.pose.position.z
+
+        self.qx = msg.poseStamped.pose.orientation.x
+        self.qy = msg.poseStamped.pose.orientation.y
+        self.qz = msg.poseStamped.pose.orientation.z
+        self.qw = msg.poseStamped.pose.orientation.w
 
 if __name__ == '__main__':
-    
+
+    armcommand = ArmCommand()
+    rospy.init_node('cart_subscriber', anonymous=True)
+    rospy.Subscriber("/iiwa_right/state/CartesianPose", CartesianPose, armcommand.callback)
+
     rospy.wait_for_service("/iiwa_left/configuration/ConfigureControlMode")
     try:
         imp = CartesianImpedanceControlMode()
@@ -20,7 +46,7 @@ if __name__ == '__main__':
         imp.cartesian_damping.a = 0.5
         imp.cartesian_damping.b = 0.5
         imp.cartesian_damping.c = 0.5
-        
+
         imp.nullspace_stiffness = 100.0
         imp.nullspace_damping = 0.7
 
@@ -30,3 +56,5 @@ if __name__ == '__main__':
 
     except rospy.ServiceException as e:
         print("Service call failed: %e"%e)
+
+    rospy.spin()
