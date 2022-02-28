30431ad93f6bf465278202402e668fee9c7ae89f
Ben Abbatematteo <abba@brown.edu>
2022-02-28 14:55:17 -0500

    add reference frame to pose message. separate impedance service call and pose command

--- cip_cartesianimpedance.py
+++ cip_cartesianimpedance.py
@@ -6,6 +6,9 @@ from geometry_msgs.msg import PoseStamped
 class ArmCommand(object):
     """docstring for ArmCommand"""
     def __init__(self):
+        
+        self.frame = None
+
         self.x = None
         self.y = None 
         self.z = None
@@ -35,6 +38,7 @@ class ArmCommand(object):
         self.qw = msg.poseStamped.pose.orientation.w
 
         if self.target_x is None:
+            self.frame = msg.poseStamped.header.frame_id
             self.target_x = self.x 
             self.target_y = self.y 
             self.target_z = self.z + 0.05
@@ -43,16 +47,14 @@ class ArmCommand(object):
             self.target_qz = self.qz
             self.target_qw = self.qw
 
-        rospy.loginfo('--')
-        rospy.loginfo('Got pose %f, %f, %f'  % (self.x, self.y, self.z))
-        rospy.loginfo('Target pose %f, %f, %f'  % (self.target_x, self.target_y, self.target_z))
-
+        
     def test_command(self):
 
         if self.target_x is None:
             return 
 
         message = PoseStamped()
+        message.header.frame_id = self.frame
 
         message.pose.position.x = self.target_x
         message.pose.position.y = self.target_y
@@ -64,44 +66,17 @@ class ArmCommand(object):
         message.pose.orientation.w = self.target_qw
         self.pub.publish(message)
 
-        
+        rospy.loginfo('-'*20)
+        rospy.loginfo('Got pose %f, %f, %f'  % (self.x, self.y, self.z))
+        rospy.loginfo('Target pose %f, %f, %f'  % (self.target_x, self.target_y, self.target_z))
+        rospy.loginfo('-'*20)
         return 
 
 if __name__ == '__main__':
-
     armcommand = ArmCommand()
-    rospy.init_node('cart_subscriber', anonymous=True)
+    rospy.init_node('arm_commander', anonymous=True)
     rospy.Subscriber("/iiwa_left/state/CartesianPose", CartesianPose, armcommand.callback)
-
-    rospy.wait_for_service("/iiwa_left/configuration/ConfigureControlMode")
-    try:
-        imp = CartesianImpedanceControlMode()
-        imp.cartesian_stiffness.x = 150.0
-        imp.cartesian_stiffness.y = 150.0
-        imp.cartesian_stiffness.z = 150.0
-        imp.cartesian_stiffness.a = 150.0
-        imp.cartesian_stiffness.b = 150.0
-        imp.cartesian_stiffness.c = 150.0
-
-        imp.cartesian_damping.x = 0.5
-        imp.cartesian_damping.y = 0.5
-        imp.cartesian_damping.z = 0.5
-        imp.cartesian_damping.a = 0.5
-        imp.cartesian_damping.b = 0.5
-        imp.cartesian_damping.c = 0.5
-
-        imp.nullspace_stiffness = 100.0
-        imp.nullspace_damping = 0.7
-
-        service = rospy.ServiceProxy("/iiwa_left/configuration/ConfigureControlMode", ConfigureControlMode)
-        result = service(control_mode=2, 
-                         cartesian_impedance=imp)
-
-    except rospy.ServiceException as e:
-        print("Service call failed: %e"%e)
-
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        rospy.loginfo("Running test command")
         armcommand.test_command()
         rate.sleep()
