d82b0611713bba9bb0cec25bad003875c4edc232
Ben Abbatematteo <abba@brown.edu>
2022-03-17 17:56:41 -0400

    links do not exist :(

--- cip_cartesianimpedance.py
+++ cip_cartesianimpedance.py
@@ -1,4 +1,5 @@
 import rospy 
+import tf2_ros
 from iiwa_msgs.msg import CartesianImpedanceControlMode, CartesianPose, JointVelocity, JointPosition
 from iiwa_msgs.srv import ConfigureControlMode
 from geometry_msgs.msg import PoseStamped
@@ -54,6 +55,13 @@ class ArmCommand(object):
 
         self.qvel_lower = SAFETY_FACTOR * np.array([-98, -98, -100, -130, -140, -180, -180])*np.pi / 180.0
         self.qvel_upper = -self.qvel_lower
+
+        # transform listener
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+        self.link_names = ["iiwa_left_link_" + str(i) for i in range(1,8)]
+        self.link_pos = {}
+
         
     def cart_callback(self, msg):
         self.frame = msg.poseStamped.header.frame_id
@@ -91,6 +99,7 @@ class ArmCommand(object):
     def check_safety(self):
         """ returns True when arm is safe"""
 
+        # check jointspace limits 
         if np.any( self.qpos < self.qpos_lower ) or np.any( self.qpos > self.qpos_upper ):
             self.safe = False
             return False 
@@ -102,6 +111,9 @@ class ArmCommand(object):
         if np.any( self.qvel < self.qvel_lower ) or np.any( self.qvel > self.qvel_upper ):
             self.safe = False
             return False 
+
+        # check workspace limits 
+        # TODO 
 
         self.last_safe_state = copy.deepcopy(self.qpos)
         return True 
@@ -155,9 +167,29 @@ class ArmCommand(object):
         message.position.a7 = q[6]
         self.pub_q.publish(message)
 
+    def update_link_poses(self):
+
+        for link_name in self.link_names:
+
+            got_transform = False
+            while not got_transform:
+                try:
+                    trans = self.tfBuffer.lookup_transform("iiwa_left_link_1", "iiwa_left_link_0", rospy.Time())
+                    self.link_pos[link_name] = trans[0]
+                    got_transform = True 
+                except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+                    print(e)
+
+        print(self.link_pos)
+                
+
+
 if __name__ == '__main__':
-    armcommand = ArmCommand()
+    
     rospy.init_node('arm_commander', anonymous=True)    
+
+    armcommand = ArmCommand()
+
     rospy.Subscriber("/iiwa_left/state/CartesianPose", CartesianPose, armcommand.cart_callback)
     rospy.Subscriber("/iiwa_left/joint_states", JointState, armcommand.joint_callback)
     rospy.Subscriber("/iiwa_left/state/JointVelocity", JointVelocity, armcommand.joint_vel_callback)
@@ -168,12 +200,15 @@ if __name__ == '__main__':
     rate = rospy.Rate(CONTROL_FREQ)
     while not rospy.is_shutdown():
 
-        if armcommand.check_safety():
-            armcommand.publish_q(target_q)
-        else:
-            if last_safe_state is None:
-                last_safe_state = copy.deepcopy(armcommand.qpos)
+        armcommand.update_link_poses()
 
-            armcommand.publish_q(last_safe_state)
-            rospy.loginfo(armcommand.qpos)
+        # if armcommand.check_safety():
+        #     armcommand.publish_q(target_q)
+        # else:
+        #     if last_safe_state is None:
+        #         last_safe_state = copy.deepcopy(armcommand.qpos)
+
+        #     armcommand.publish_q(last_safe_state)
+        #     rospy.loginfo(armcommand.qpos)
+
         rate.sleep()
